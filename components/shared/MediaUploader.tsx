616a526d762c8715b573f321234ae8f00ad26005
zinminoo33 <zinminoo.min.zin@gmail.com>
2025-02-18 15:57:05 +0700

    final commit

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="imaginify_zmo"
       options={{
         multiple: false,
         resourceType: "image",
